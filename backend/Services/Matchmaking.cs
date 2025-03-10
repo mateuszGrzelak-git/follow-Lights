7883cc77adfc78c16617c3b96042ee8d03b8f4a3
mateuszGrzelak-git <grzelak03mateusz@gmail.com>
2025-03-10 21:14:49 +0100

    Added simplified matchmaking

---
+++ backend/Services/Matchmaking.cs
@@ -0,0 +1,27 @@
+﻿using backend.Domain;
+
+namespace backend.Services
+{
+    public class Matchmaking
+    {
+        UserProgress[] userProgress;
+
+        public Matchmaking(UserProgress[] userProgress)
+        {
+            this.userProgress = userProgress;
+        }
+
+        public void findMatch(UserProgress first)
+        {
+            UserProgress[] potentialOpponents = new UserProgress[userProgress.Length];
+
+            foreach (var item in userProgress) {
+            {
+                    //if (item.isActive())
+                    {
+                        potentialOpponents.Append(item);
+                    }
+            }
+        }
+    }
+}
